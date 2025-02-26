124988594c5919afd7aa70b305aa2e490d5de592
Deva P <devasaikrishna@gmail.com>
2025-02-26 00:19:49 -0500

    profile updates

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -8,10 +8,13 @@ const Intro = () => {
         <div class="introContent">
             <span className='hello'>Hello,</span>
             <span className='introText'>I'm
-                <span className='introName'> Deva</span> <br />Software Engineer
+                <span className='introName'> Deva Potarlanka</span> <br />Software Engineer
             </span>
             <p className='introPara'>I'm a skilled Software Engineer with experience in designing, developing, deploying and maintainance of applications.</p>
-            <Link><button className='btn'>Hire Me</button></Link>
+            <div className='introBtn'>
+              <Link><button className='btn'>Hire Me</button></Link>
+              <Link><button className='btn'>View Resume</button></Link>
+            </div>
         </div>
         <img src={`${process.env.PUBLIC_URL}/assets/Main_logo.png`} alt="Profile" class="bg" />
     </section>
