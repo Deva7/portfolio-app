34bbf3acdfb92a79579479247ef367e2b4559f5b
Deva P <devasaikrishna@gmail.com>
2025-02-26 23:09:26 -0500

    intro btn enabled

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -3,6 +3,9 @@ import './intro.css'
 import { Link } from 'react-scroll'
 
 const Intro = () => {
+  const handleClick = () => {
+    window.open('https://drive.google.com/file/d/1rVs_lAbugCC6vssm7N2oiJsinjfDf57Y/view?usp=drive_link', '_blank');
+  };
   return (
     <section id="intro">
         <div class="introContent">
@@ -12,8 +15,8 @@ const Intro = () => {
             </span>
             <p className='introPara'>I'm a skilled Software Engineer with experience in designing, developing, deploying and maintainance of applications.</p>
             <div className='introBtn'>
-              <Link><button className='btn'>Hire Me</button></Link>
-              <Link><button className='btn'>View Resume</button></Link>
+              <Link activeClass='active' to='contact-section' spy={true} smooth={true} offset={50} duration={500}><button className='btn'>Hire Me</button></Link>
+              <Link onClick={handleClick} target='_blank'><button className='btn'>View Resume</button></Link>
             </div>
         </div>
         <img src={`${process.env.PUBLIC_URL}/assets/Main_logo.png`} alt="Profile" class="bg" />
