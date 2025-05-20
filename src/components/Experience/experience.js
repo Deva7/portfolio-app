ebffc923d62d44f3ea08c8b86bb290f6890b9127
Deva P <devasaikrishna@gmail.com>
2025-05-20 14:04:25 -0400

    experience updated

--- src/components/Experience/experience.js
+++ src/components/Experience/experience.js
@@ -5,15 +5,17 @@ import './experience.css';
 const experiences = [
   {
     role: 'API Lead Developer',
-    company: 'Sherwin-Williams',
+    company: 'Squad Software Inc.',
     responsibilities: [
       'Designing, Developed, Led, and maintained secure microservices.',
       'Developed RESTful and GraphQL APIs.',
       'Improving authentication security through ABAC with Open Policy Agent (OPA) and OAuth 2.0.',
-      'Managed CI/CD pipelines, code reviews, and team collaboration.'
+      'Managed CI/CD pipelines, code reviews, and team collaboration.',
+      'Implemented Cloud Architecture for deploying microservices and ETL Processes',
+      'Created Oracle Database views and used Oracle for all database config.'
     ],
-    startDate: 'Dec 2023',
-    endDate: 'Sep 2024',
+    startDate: 'Aug 2023',
+    endDate: 'Present',
     techStack: ['python', 'fastapi', 'sqlalchemy', 'openapi', 'oracle', 'githubactions', 'docker', 'ansible', 'kubernetes', 'azure', 'amazonwebservices', 'jira']
   },
   {
@@ -23,7 +25,7 @@ const experiences = [
       'Designed, developed, and deployed web and mobile applications ',
       'Implemented CMS for backend application and optimized performance.',
       'Developed PaaS and SaaS solutions for clients.',
-      'Deployed applications on AWS Cloud.',
+      'Deployed applications on AWS Cloud, utilized variety of services.',
       'Configured Atlas MongoDB cloud for database migration.',
     ],
     startDate: 'Jul 2018',
@@ -36,9 +38,10 @@ const experiences = [
     responsibilities: [
       'Automated system tasks using shell scripts.',
       'Managed and maintained Linux servers.',
-      'Configured and managed MySQL databases.',
+      'Configured and managed Oracle databases.',
       'Implemented security measures and monitored system performance.',
-      'Provided technical support and troubleshooting.'
+      'Provided technical support and troubleshooting.',
+      'Designed and developed web app using Python'
     ],
     startDate: 'Jul 2016',
     endDate: 'Jul 2018',
