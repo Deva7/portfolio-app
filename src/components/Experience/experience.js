e9b2341c9988d12daa6eb43dd9532567d642afd4
Deva P <devasaikrishna@gmail.com>
2026-01-19 20:20:14 -0500

    exp update and button fix

--- src/components/Experience/experience.js
+++ src/components/Experience/experience.js
@@ -4,8 +4,22 @@ import './experience.css';
 
 const experiences = [
   {
+    role: 'Python AWS Developer',
+    company: 'J.P Morgan Chase',
+    responsibilities: [
+      'Designing, Developing, and maintaining Infra setup and device data APIs.',
+      'Improving security through Terraform provisioning restricting access to AWS Console.',
+      'Managing CI/CD pipelines, code reviews, and team collaboration.',
+      'Designed Cloud Architecture for deploying lambda APIs.',
+      'DynamoDB Database configuration setup within AWS Infra with alerting.'
+    ],
+    startDate: 'Nov 2025',
+    endDate: 'Present',
+    techStack: ['python', 'openapi', 'github', 'amazonwebservices', 'jira', 'Terraform']
+  },
+  {
     role: 'Python Lead API Developer',
-    company: 'Sherwin Williams',
+    company: 'Sherwin-Williams',
     responsibilities: [
       'Designing, Developed, Led, and maintained secure microservices.',
       'Developed APIs.',
@@ -15,52 +29,37 @@ const experiences = [
       'Created Oracle Database views and used Oracle for all database config.'
     ],
     startDate: 'Dec 2023',
-    endDate: 'Present',
+    endDate: 'Sep 2024',
     techStack: ['python', 'fastapi', 'sqlalchemy', 'openapi', 'oracle', 'githubactions', 'docker', 'ansible', 'kubernetes', 'azure', 'amazonwebservices', 'jira']
   },
   {
-    role: 'Senior Python Developer',
-    company: 'BCBS',
-    responsibilities: [
-      'Worked on Python application development, ensuring high-quality, efficient, and maintainable code.',
-      'Utilized Djangos MVT architecture to build robust web applications with RESTful APIs.',
-      'Managed PostgreSQL and MongoDB databases, including data modeling, querying, and optimization.',
-      'Collaborated with front-end developers to create responsive web interfaces using HTML, CSS, and JavaScript.',
-      'Implemented data interchange between systems using AJAX, XML, and JSON.',
-      'Developed and deployed microservices using Python, Flask, and FastAPI frameworks, promoting modular and scalable architecture.',
-      'Migrated monolithic applications to microservices, decoupling tightly coupled dependencies into independent services.',
-      'Documented application architectures, data models, APIs.'
-    ],
-    startDate: 'Aug 2022',
-    endDate: 'Dec 2023',
-    techStack: ['python', 'fastapi', 'sqlalchemy', 'openapi', 'oracle', 'githubactions', 'docker', 'ansible', 'kubernetes', 'azure', 'amazonwebservices', 'jira']
-  },
-  {
-    role: 'Python Developer',
-    company: 'Gridlogic',
+    role: 'Software Engineer',
+    company: 'UiRevolution India Pvt. Ltd.',
     responsibilities: [
       'Designed, developed, and deployed web and mobile applications ',
       'Implemented CMS for backend application and optimized performance.',
       'Developed PaaS and SaaS solutions for clients.',
       'Deployed applications on AWS Cloud, utilized variety of services.',
-      'Configured Atlas MongoDB cloud for database migration.',
+      'Configured Atlas MongoDB cloud for database migration.'
     ],
     startDate: 'Jul 2018',
-    endDate: 'Nov 2021',
-    techStack: ['html5', 'css3', 'javascript', 'figma', 'nodejs', 'vuejs', 'react', 'python', 'mongodb', 'microsoftsqlserver', 'githubactions', 'bitbucket', 'docker', 'amazonwebservices', 'jira', 'salesforce', 'zohocreator']
+    endDate: 'Feb 2021',
+    techStack: ['html5', 'css3', 'javascript', 'figma', 'nodejs', 'vuejs', 'react', 'python', 'mongodb', 'microsoftsqlserver', 'githubactions', 'bitbucket', 'docker', 'amazonwebservices', 'jira', 'salesforce', 'zohocreator']  
   },
   {
-    role: 'Python Developer',
-    company: 'Infronics Systems Ltd.,',
+    role: 'System Administrator',
+    company: 'ITR Infratech',
     responsibilities: [
-      'Designed, implemented, and tested Python-based solutions leveraging object-oriented programming (OOP) principles to meet business requirements.',
-      'Developed backend components using the Django framework, utilizing Object-Relational Mapping (ORM) to streamline database interaction and optimize data handling.',
-      'Developed and deployed RESTful APIs to ensure smooth communication between application components.',
-      'Optimized SQL Server database schemas to improve data storage and retrieval performance.'
+      'Automated system tasks using shell scripts.',
+      'Managed and maintained Linux servers.',
+      'Configured and managed Oracle databases.',
+      'Implemented security measures and monitored system performance.',
+      'Provided technical support and troubleshooting.',
+      'Designed and developed web app using Python'  
     ],
-    startDate: 'Aug 2015',
+    startDate: 'Jul 2016',
     endDate: 'Jul 2018',
-    techStack: ['python', 'react', 'git', 'heroku', 'mysql', 'linux', 'jira']
+    techStack: ['python', 'react', 'git', 'heroku', 'Oracle', 'linux', 'jira']
   }
 ];
 
@@ -71,8 +70,19 @@ const Experience = () => {
         <h1>Experience</h1>
         {experiences.map((exp, index) => (
           <div key={index} className="experience-card">
-            <h2>{exp.role} at {exp.company}</h2>
+            {/* <h2>{exp.role} at {exp.company}</h2>
             <p><strong>{exp.startDate} - {exp.endDate}</strong></p>
+            <ul>
+              {exp.responsibilities.map((resp, i) => (
+                <li key={i}>{resp}</li>
+              ))}
+            </ul> */}
+            <div class="role-company">{exp.company}
+              <div class="role-header">
+                <span class="role-name">{exp.role}</span>
+                <span class="date-range">{exp.startDate} - {exp.endDate}</span>
+              </div>
+            </div>
             <ul>
               {exp.responsibilities.map((resp, i) => (
                 <li key={i}>{resp}</li>
